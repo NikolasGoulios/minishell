53cfa2e0c67c183d5f8975867fa774ac7209830d
Seela Salorinta <seelasalorinta@Seela-MacBook-Pro.local>
2025-01-17 18:48:05 +0200

    [HEADER]: added more prototypes

--- seela.h
+++ seela.h
@@ -11,5 +11,9 @@
 void	ft_command(char **envp, char *cmd);
 void	ft_print_err(char *cmd, int c);
 void	ft_free_array(char **array);
+void	redirection_outfile_emptied(char *file);
+void	redirection_infile(char *file);
+void	redirection_outfile_append(char *file);
+void    handle_heredoc(char *limiter);
 
 #endif
