70ce4c1e295c8399df18a56647dbb22cf4b3c5a5
Nikolas Goulios <ngoulios@outlook.com>
2025-01-24 19:25:48 +0200

    [STRUCTS]

--- include/parsing.h
+++ include/parsing.h
@@ -6,7 +6,7 @@
 /*   By: ngoulios <ngoulios@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 21:33:17 by ngoulios          #+#    #+#             */
-/*   Updated: 2025/01/15 20:17:59 by ngoulios         ###   ########.fr       */
+/*   Updated: 2025/01/24 19:24:56 by ngoulios         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,50 +28,78 @@ user inputs from a history list. It supports navigating through input
 history and persisting it across sessions*/
 # include <readline/history.h>
 
-/**
- * @enum TokenType
- * @brief Represents the different types of tokens in a lexical analyzer.
- *
- * This enumeration defines all possible categories a token can fall into
- * during lexical analysis. It is used to identify and classify pieces of
- * input text, improving readability and facilitating further parsing or
- * processing.
- *
- * @var TOKEN_KEYWORD
- *      A reserved word in the language (e.g., "int", "return").
- *
- * @var TOKEN_IDENTIFIER
- *      A name defined by the user (e.g., variable or function names).
- *
- * @var TOKEN_NUMBER
- *      A numeric literal (e.g., "10", "3.14").
- *
- * @var TOKEN_OPERATOR
- *      An operator used in expressions (e.g., "+", "-", "=").
- *
- * @var TOKEN_SEPARATOR
- *      A symbol that separates statements or elements (e.g., ";", ",").
- */
-typedef enum
-{
-	TOKEN_WORD, // 0
-    TOKEN_PIPE, // 1
-    TOKEN_REDIRECT_IN, //2
-    TOKEN_REDIRECT_OUT, //3
-    TOKEN_APPEND, //4
-    TOKEN_HEREDOC,// 5
-    TOKEN_SEPARATOR, //6 
-	TOKEN_COMMAND, // 7 The command (e.g., "ls")
-	TOKEN_ARGUMENT, // 8 Arguments (e.g., "-la")
-    TOKEN_UNRECOGNIZED // 9 For anything unexpected
-}					TokenType;
 
-typedef struct	t_token
+typedef enum token_type
 {
-	TokenType		type;
-	char			*value;
-	struct t_token	*prev;
-	struct t_token	*next;
-}					s_token;
+	NONE = 0,
+	CMD = 1,
+	PIPE = 2,
+	OR = 3,
+	AND = 4,
+	BR_OPEN = 5,
+	BR_CLOSE = 6,
+}	t_token_type;
+
+typedef enum redirect_type
+{
+	RD_IN = 0,
+	RD_OUT = 1,
+	RD_HEREDOC = 2,
+	RD_APPEND = 3,
+}	t_redirect_type;
+
+typedef enum e_error
+{
+	ERR_NONE = 0,
+	ERR_SYNTAX = 1,
+	ERR_MALLOC = 2,
+	ERR_FORK = 3,
+	ERR_PIPE = 4
+}	t_error;
+
+typedef enum e_syntax
+{
+	SYN_NONE = 0,
+	SYN_QUOTE = 1,
+	SYN_RD = 2,
+	SYN_LOGIC = 3,
+	SYN_BR = 4,
+	SYN_NORMAL = 5
+}	t_syntax;
+
+//----------------------------------------------------
+// TOKEN 
+//-----------------------------------------------------
+
+typedef struct s_redir
+{
+	int					fd; // File Descriptor for redirection
+	int					no_exp; // Flag indicating expansion is off (heredocs)
+	t_redirect_type		type; // Type of redirection
+	char				*orig_path; // Original path in the command
+	char				*path; // Processed or resolved file path for the redirection
+	struct s_redir		*next; // Pointer to the next redirection in a linked list
+}	t_redir;
+
+typedef struct s_token
+{
+	t_token_type		type; // Type of token
+	char				*cmd; // The actual command or token string
+	char				**args; // Arguments for the command
+	t_redir				*redir; // Pointer to the list of redirections 
+}	t_token;
+
+typedef struct s_token_list
+{
+	t_token				*array; // Array of tokens
+	size_t				size; // Total size of the token array
+	size_t				cur_pos; // Current position
+	size_t				to_add; // Number of tokens to be added
+	int					br_open; // Count of currently open brackets
+	int					has_heredoc; // Flag indicating if the command contains a heredoc
+	int					has_pipe; // Flag indicating if the command contains a pipe
+	char				*last_cmd; // Stores the result of the last executed command
+	t_syntax			syntax_err; // Stores syntax error
+}	t_token_list;
 
 #endif
