da4aebc97c267b1b19dbe421f5e9091ef417af79
Seela Salorinta <seelasalorinta@Seela-MacBook-Pro.local>
2025-01-25 15:59:04 +0200

    [EXPORT_ENV]: export and env handling

--- initialize_struct.c
+++ initialize_struct.c
@@ -1,31 +1,5 @@
 #include "seela.h"
 
-char	**copy_map(char **original_map)
-{
-	char	**new_map;
-    int     i;
-
-    i = 0;
-    while (original_map[i])
-        i++;
-	new_map = malloc(sizeof(char *) * (i + 1));
-	if (!new_map)
-		return (NULL);
-    i = 0;
-	while (original_map[i])
-	{
-		new_map[i] = ft_strdup(original_map[i]);
-		if (!new_map[i])
-		{
-			ft_free_map(new_map);
-			return (NULL);
-		}
-		i++;
-	}
-	new_map[i] = NULL;
-	return (new_map);
-}
-
 void    initialize_struct(char **envp)
 {
     t_ms ms;
--- seela.h
+++ seela.h
@@ -15,15 +15,41 @@ typedef struct s_ms
     char    **exported;
 } t_ms;
 
+//env_export2.c
+void    handle_env(t_ms *ms); //ENV HANDLING
+void    print_exported(t_ms *ms);
+void    sort_exported_alphaorder(t_ms *ms);
+
+//env_export.c
+void    handle_export(char **args, t_ms *ms); //EXPORT HANDLING
+void    add_to_exported_env(char *arg, t_ms *ms);
+void    add_to_env(char *arg, t_ms *ms, char *name, int len);
+void    add_to_exported(char *arg, t_ms *ms, char *name, int len);
+void    update_exported(char *arg, t_ms *ms)
+
+//double_array_handling.c
 char	**copy_map(char **original_map);
 void	ft_command(char **envp, char *cmd);
+void    print_array(char **m);
+
+//initialize_struct.c
 void    initialize_struct(char **envp);
+
+//cmd_errormsg_free.c
 void	ft_print_err(char *cmd, int c);
 void	ft_free_array(char **array);
-void    ft_pipe(int num_cmds, char **argv, t_ms *ms);
-void	redirection_outfile_emptied(char *file);
-void	redirection_infile(char *file);
-void	redirection_outfile_append(char *file);
-void    handle_heredoc(char *limiter);
+void    print_array(char **m);
+
+//find_path_and_execute_command.c
+void	ft_command(char **envp, char *cmd); //EXECVE HANDLING
+
+//pipe.c
+void    ft_pipe(int num_cmds, char **argv, t_ms *ms); //PIPE HANDLING
+
+//redrections.c
+void	redirection_outfile_emptied(char *file); //< HANDLING
+void	redirection_infile(char *file); //> HANDLING
+void	redirection_outfile_append(char *file); //<< HANDLING
+void    handle_heredoc(char *limiter); //>> HANDLNG
 
 #endif
