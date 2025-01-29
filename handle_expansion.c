25d340a14f5d58937b92c9ac57d3ed898f2595ad
Seela Salorinta <seelasalorinta@Seela-MacBook-Pro.local>
2025-01-29 21:08:55 +0200

    [EDGE CASE]: handle 742 and 0

--- handle_expansion.c
+++ handle_expansion.c
@@ -1,13 +1,17 @@
 /*
 handles $USER, so enviroment variable expansion
 args has all the arrays need to bee expanded
-for example args = "My home directory is $HOME" or args="$USER$LOGNAME"
+for example args = "My home directory is $HOME" or args="$USER$LOGNAME" or args="THis is $INVALIDKEY"
+"My home directory is /Users/seela", "seelasalorintaseela", "THis is  "
 if the KEY dont exists replace the $HOME with "", else replace $HOME with the VALUE
 
 KEY only contains numbers, alphabets or _ and the first character cant be a number
-*/
 
-#include "seela.h"
+edge cases if KEY is $? , replace the KEY with latest exit_value
+"This is $?" = "This is 127"
+edge case if KEY is $$, replace the KEy with 1140
+"This is $$" "This is 1140"
+*/
 
 #include "seela.h"
 
@@ -18,12 +22,16 @@ static char	*verify_key(char *key)
 	return (ft_strdup(key));
 }
 
-char	*expand_key(char **envp, char *key, int len)
+char	*expand_key(char **envp, char *key, int len, t_ms *ms)
 {
 	int		i;
 	char	*check;
 
 	i = 0;
+	if (key[0] == '?')
+		return(ft_itoa(ms->exit_status));
+	if (key[0] == '$')
+		return (ft_itoa(1140));
 	check = verify_key(key);
 	if (!check)
 		return (ft_strdup(""));
@@ -59,7 +67,7 @@ char	*handle_expansion(char *args, t_ms *ms)
 		{
 			i++;
 			x = 0;
-			if (ft_isdigit(args[i])) // Stop if it starts with a number
+			if (ft_isdigit(args[i]) || args[i] == '?' || args[i] == '$') // Stop if it starts with a number
 				x = 1;
 			else
 			{
@@ -67,7 +75,7 @@ char	*handle_expansion(char *args, t_ms *ms)
 					x++;
 			}
 			key = ft_substr(args, i, x);
-			s = expand_key(ms->envp, key, x);
+			s = expand_key(ms->envp, key, x, ms);
 			temp = ft_strjoin(string, s);
 			free(string);
 			string = temp;
